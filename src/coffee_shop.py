5879cf9492bbc331b9655703d907abdc65d5dd72
Louise McCafferty <mccafferty_@hotmail.com>
2022-10-19 01:41:42 +0100

    final commit

---
+++ src/coffee_shop.py
@@ -0,0 +1,20 @@
+ class CoffeeShop:
+    def __init__(self, name, till):
+        self.name = name
+        self.till = till
+        self.drinks = []
+
+    def increase_till(self, amount):
+        self.till += amount
+
+    def check_customer_age(self, customer_age):
+        return customer_age >= 16
+
+    def check_energy_level(self, customer_energy_level):
+        return customer_energy_level < 10
+        
+    def stock_value(self, drinks_list):
+        total_value = 0
+        for drink in drinks_list:
+            total_value += drink["beverage"].price * drink["stock"]
+        return total_value
